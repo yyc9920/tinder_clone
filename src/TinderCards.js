da98bd2750bdb33f21005c262b635a79cfa89928
Kendrick Yun <yyc837021@gmail.com>
2022-01-09 00:32:42 +0900

    added tinder cards

---
+++ src/TinderCards.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
+
+console.log(TinderCard);
+
+function TinderCards() {
+    const [people, setPeople] = useState([
+        {
+            name: "Elon Musk",
+            url: "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+        },
+        {
+            name: "Jeff Bezos",
+            url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY2NzA3ODE3OTgwMzcyMjYw/jeff-bezos-andrew-harrer_bloomberg-via-getty-images.jpg",
+        }
+    ]);
+
+    const swiped = (direction, nameToDelete) => {
+        console.log("removing: " + nameToDelete);
+        // setLastDirection(direction);
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name + " left the screen!");
+    }
+
+    return (
+        <div className="tinderCards">
+            <div className="tinderCards__cardContainer">
+                {people.map((person) => (
+                    <TinderCard
+                        className="swipe"
+                        key={person.name}
+                        preventSwipe={["up", "down"]}
+                        onSwipe={(dir) => swiped(dir, person.name)}
+                        onCardLeftScreen={() => outOfFrame(person.name)}
+                    >
+                        <div
+                            style={{ backgroundImage: `url(${person.url})` }}
+                            className="card"
+                        >
+                            <h3>{person.name}</h3>
+                        </div>
+                    </TinderCard>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default TinderCards;
